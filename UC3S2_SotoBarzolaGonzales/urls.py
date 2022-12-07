0789db8adbf032207c19cefe02dae5501636b9b0
Danee Barzola Navarro <1923010381@untels.edu.pe>
2022-12-07 10:20:52 -0500

    UC3S3_Parte02_BarzolaNavarro

--- UC3S2_SotoBarzolaGonzales/urls.py
+++ UC3S2_SotoBarzolaGonzales/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', views.inicio, name="inicio"),
+    path('',views.inicio, name="index"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+layout = """
+    <h3>
+        <ul>
+            <li>
+                <a href="inicio/">Inicio</a>
+            </li>
+        </ul>
+    </h3>
+    <hr>
+"""
+def inicio(request):
+    mensaje = """
+        <h1>Lenguaje de programación III</h1>
+        <h2>Integrantes:</h2>
+        <h2>
+            <ul>
+                <li>Barzola Navarro, Danee</li>
+                <li>Gonzales Castillo, Daniel</li>
+                <li>Soto Obregón, Milagros Katherine</li>
+            </ul>
+        </h2>
+    """
+    return HttpResponse(layout+mensaje)
+
